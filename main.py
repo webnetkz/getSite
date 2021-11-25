7b1680fdc438a3edc4bc3420362b7b2483eedf61
webnetkz <dbqqbq@gmail.com>
2021-11-25 10:45:56 +0600

    update

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+def getSite(url):
+    r = requests.get(url).text
+    file = open('index.html', 'w')
+    #print(r.text)
+    file.write(r)
+    #file.close()
+
+getSite('http://ps.kz')
